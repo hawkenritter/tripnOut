dfe3ae60fb0e91b0f914a863d084d68fb7f67967
Hawken Ritter <hawkenritter@gmail.com>
2017-09-19 20:35:16 -0700

    Add model and controller for user auth

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,30 @@
+class TripsController < ApplicationController
+
+  def index
+    @trips = Trip.all
+    render json: @trips, status: :ok
+  end
+
+  def create
+    @trip = Trip.new(trip_params).first
+
+    @trip.save
+    render json: @trip, status: :created
+  end
+
+  def destroy
+
+    @trip = Trip.where(id: params[:id]).first
+
+    if @trip.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+    def trip_params
+      params.require(:trip).permit(:first_name, :last_name, :email)
+    end
+end
